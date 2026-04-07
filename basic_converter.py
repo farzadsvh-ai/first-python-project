1986c9abdbe9b439a211baec7f9d7707ffb4cea6
farzad-ai <farzad1siavoshi@gmail.com>
2026-04-07 13:52:32 +0330

    Create basic_converter.py

---
+++ basic_converter.py
@@ -0,0 +1,8 @@
+# Simple Python Script by Farzad - Rome
+# This script converts Kilometers to Miles
+
+km = float(input("Enter distance in Kilometers: "))
+ratio = 0.621371
+miles = km * ratio
+
+print(f"{km} Kilometers is equal to {miles:.2f} Miles.")
